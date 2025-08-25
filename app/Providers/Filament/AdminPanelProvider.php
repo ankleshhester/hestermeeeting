734317c2b87a169ca77f06002fb3696e46005d71
anklesh-chauhan <your-email@example.com>
2025-08-25 15:43:45 +0530

    1234

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
-            ->homeUrl(fn () => route('filament.admin.resources.meetings.index'))
+            ->homeUrl('/admin/meetings')
             ->colors([
                 'primary' => Color::Amber,
             ])
